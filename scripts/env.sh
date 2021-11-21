15083e4bd8df473cf98fb1a16be0e1c2e29ad10b
Elbtalkessel <me@0x00.life>
2021-11-21 17:35:43 +0100

    update

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 # shellcheck disable=SC2034
 
-CPU_HOST="host,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on"
-CPU_PENRYN="Penryn,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on,+ssse3,+sse4.2,+popcnt,+avx,+aes,+xsave,+xsaveopt,check"
-#CPU_PENRYN="Penryn,vendor=GenuineIntel,+hypervisor,+invtsc,kvm=on,+fma,+avx,+avx2,+aes,+ssse3,+sse4.2,+popcnt,+sse4a,+bmi1,+bmi2"
-CPU_OPTIONS=$CPU_PENRYN
+CPU_OPTIONS="Penryn,kvm=on,vendor=GenuineIntel,+invtsc,vmware-cpuid-freq=on,+ssse3,+sse4.2,+popcnt,+avx,+aes,+xsave,+xsaveopt,check"
 
 ALLOCATED_RAM="8192" # MiB
 CPU_SOCKETS="1"
@@ -18,7 +15,7 @@ EFI_DIR="$BASE_DIR/EFI"
 BOOTLOADER="$BASE_DIR/OpenCore.qcow2"
 INSTALLER="$BASE_DIR/BaseSystem.img"
 SYSTEM="$BASE_DIR/System.qcow2"
-OVMF_CODE="$BASE_DIR/OVMF_CODE.fd"
+OVMF_CODE="../firmware/OVMF_CODE_PATCHED.fd"
 OVMF_VARS="$BASE_DIR/OVMF_VARS-1024x768.fd"
 BOOT_MOUNT="/media"
 
